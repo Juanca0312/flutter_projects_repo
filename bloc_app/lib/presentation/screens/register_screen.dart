90fe00b610e9d1df84a2f57fd51238e7ad28a529
Juan Carlos Hernández <jchernandez0312@gmail.com>
2023-12-21 10:39:22 -0500

    bloc_app - added custom text form field

--- bloc_app/lib/presentation/screens/register_screen.dart
+++ bloc_app/lib/presentation/screens/register_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_app/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatelessWidget {
@@ -23,11 +24,69 @@ class _RegisterView extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: SingleChildScrollView(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [TextFormField()]),
+          child: Column(children: [
+            const FlutterLogo(
+              size: 100,
+            ),
+            _RegisterForm(),
+            const SizedBox(height: 20),
+          ]),
         ),
       ),
+    );
+  }
+}
+
+class _RegisterForm extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(children: [
+        CustomTextFormField(
+          label: 'nombre de usuario',
+          validator: (value) {
+            if (value == null || value.isEmpty) return 'Campo requerido';
+            if (value.trim().isEmpty) return 'Campo requerido';
+            if (value.length < 6) return 'Usuario mayor a 6 letras';
+            return null;
+          },
+        ),
+        const SizedBox(height: 10),
+        CustomTextFormField(
+          label: 'correo electrónico',
+          validator: (value) {
+            if (value == null || value.isEmpty) return 'Campo requerido';
+            if (value.trim().isEmpty) return 'Campo requerido';
+            final emailRegExp = RegExp(
+              r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+            );
+            if (!emailRegExp.hasMatch(value))
+              return 'No tiene formato de correo';
+            return null;
+          },
+        ),
+        const SizedBox(height: 10),
+        CustomTextFormField(
+          label: 'password',
+          obscureText: true,
+          validator: (value) {
+            if (value == null || value.isEmpty) return 'Campo requerido';
+            if (value.trim().isEmpty) return 'Campo requerido';
+            if (value.length < 6) return 'Password mayor a 6 letras';
+            return null;
+          },
+        ),
+        const SizedBox(height: 20),
+        FilledButton.tonalIcon(
+            onPressed: () {
+              final isValid = _formKey.currentState!.validate();
+            },
+            icon: const Icon(Icons.save),
+            label: const Text('Save user')),
+      ]),
     );
   }
 }
---
+++ bloc_app/lib/presentation/widgets/inputs/custom_text_form_field.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? label;
+  final String? hint;
+  final String? errorMessage;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  final bool? obscureText;
+
+  const CustomTextFormField(
+      {super.key,
+      this.label,
+      this.hint,
+      this.errorMessage,
+      this.onChanged,
+      this.validator,
+      this.obscureText});
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+    final border = OutlineInputBorder(borderRadius: BorderRadius.circular(20));
+
+    return TextFormField(
+      onChanged: onChanged,
+      validator: validator,
+      obscureText: obscureText ?? false,
+      decoration: InputDecoration(
+        enabledBorder: border,
+        focusedBorder:
+            border.copyWith(borderSide: BorderSide(color: color.primary)),
+        errorBorder:
+            border.copyWith(borderSide: BorderSide(color: Colors.red.shade800)),
+        focusedErrorBorder:
+            border.copyWith(borderSide: BorderSide(color: Colors.red.shade800)),
+        isDense: true,
+        label: label != null ? Text(label!) : null,
+        hintText: hint,
+        errorText: errorMessage,
+        focusColor: color.primary,
+      ),
+    );
+  }
+}
