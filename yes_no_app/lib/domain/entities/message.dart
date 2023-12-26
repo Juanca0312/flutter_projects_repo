3d20318569fde38666848098dd39b983e7b95d18
Juan Carlos Hernández <jchernandez0312@gmail.com>
2023-12-26 13:55:10 -0500

    yes_no_app - added message entity

---
+++ yes_no_app/lib/domain/entities/message.dart
@@ -0,0 +1,13 @@
+enum FromWho { mine, hers }
+
+class Message {
+  final String text;
+  final String? messageUrl;
+  final FromWho fromWho;
+
+  Message({
+    required this.text,
+    this.messageUrl,
+    required this.fromWho,
+  });
+}
