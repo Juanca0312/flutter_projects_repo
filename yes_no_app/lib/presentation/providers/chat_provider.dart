6e2ebe79ea30382282035eec853ecbdeaf02a0b8
Juan Carlos Hernández <jchernandez0312@gmail.com>
2024-01-03 17:10:38 -0500

    yes_no_app - implemented call to api with dio package

--- yes_no_app/lib/presentation/providers/chat_provider.dart
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messages = [
     Message(text: 'habla p bateria', fromWho: FromWho.me),
@@ -13,6 +15,19 @@ class ChatProvider extends ChangeNotifier {
     if (text.isEmpty) return;
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messages.add(newMessage);
+
+
+    if (text.endsWith('?')) {
+      herReply();
+    }
+
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+    messages.add(herMessage);
     notifyListeners();
     moveScrollToBottom();
   }
