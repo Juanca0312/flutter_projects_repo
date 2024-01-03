176055e92062e4cf40fd5c9cd2e549487bcc6409
Juan Carlos Hernández <jchernandez0312@gmail.com>
2024-01-03 17:45:58 -0500

    bloc_app - connected screen with register form cubit

--- bloc_app/lib/presentation/screens/register_screen.dart
+++ bloc_app/lib/presentation/screens/register_screen.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_app/presentation/blocs/register_cubit/register_cubit.dart';
 import 'package:bloc_app/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -10,7 +12,10 @@ class RegisterScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('new user'),
       ),
-      body: const _RegisterView(),
+      body: BlocProvider(
+        create: (context) => RegisterCubit(),
+        child: const _RegisterView(),
+      ),
     );
   }
 }
@@ -42,10 +47,16 @@ class _RegisterForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final registerCubit = context.watch<RegisterCubit>();
+
     return Form(
       key: _formKey,
       child: Column(children: [
         CustomTextFormField(
+          onChanged: (value) {
+            registerCubit.usernameChanged(value);
+            _formKey.currentState!.validate();
+          },
           label: 'nombre de usuario',
           validator: (value) {
             if (value == null || value.isEmpty) return 'Campo requerido';
@@ -56,6 +67,10 @@ class _RegisterForm extends StatelessWidget {
         ),
         const SizedBox(height: 10),
         CustomTextFormField(
+          onChanged: (value) {
+            registerCubit.emailChanged(value);
+            _formKey.currentState!.validate();
+          },
           label: 'correo electrónico',
           validator: (value) {
             if (value == null || value.isEmpty) return 'Campo requerido';
@@ -63,13 +78,18 @@ class _RegisterForm extends StatelessWidget {
             final emailRegExp = RegExp(
               r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
             );
-            if (!emailRegExp.hasMatch(value))
+            if (!emailRegExp.hasMatch(value)) {
               return 'No tiene formato de correo';
+            }
             return null;
           },
         ),
         const SizedBox(height: 10),
         CustomTextFormField(
+          onChanged: (value) {
+            registerCubit.passwordChanged(value);
+            _formKey.currentState!.validate();
+          },
           label: 'password',
           obscureText: true,
           validator: (value) {
@@ -83,6 +103,9 @@ class _RegisterForm extends StatelessWidget {
         FilledButton.tonalIcon(
             onPressed: () {
               final isValid = _formKey.currentState!.validate();
+              if (!isValid) return;
+
+              registerCubit.onSubmit();
             },
             icon: const Icon(Icons.save),
             label: const Text('Save user')),
